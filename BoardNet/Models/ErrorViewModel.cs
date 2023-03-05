dace79fe403c1d7e67266bc4c3e6edfac9821936
ljh9 <dkssud2208@naver.com>
2023-03-05 21:17:35 +0900

    BN-4 Add Project

---
+++ BoardNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BoardNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
